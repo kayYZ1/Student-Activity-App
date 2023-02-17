1321b8b84e5091bc0479cfdbdaec65dcea16273b
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-02-17 19:33:50 +0100

    feat: Adding events via post request
    small ui bugs left to fix

--- client/src/pages/NewEvent.tsx
+++ client/src/pages/NewEvent.tsx
@@ -1,4 +1,6 @@
 import React, { FormEvent, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 import classes from "./styles/NewEvent.module.css"
 
@@ -6,6 +8,7 @@ type Props = {
 }
 
 const NewEvent = (props: Props) => {
+	const navigate = useNavigate();
 	const titleInputRef = useRef<HTMLInputElement>(null);
 	const addressInputRef = useRef<HTMLInputElement>(null);
 	const descriptionInputRef = useRef<HTMLTextAreaElement>(null);
@@ -13,22 +16,39 @@ const NewEvent = (props: Props) => {
 	const timeInputRef = useRef<HTMLInputElement>(null);
 	const dateInputRef = useRef<HTMLInputElement>(null);
 
-	const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+	const submitHandler = (event: FormEvent) => {
 		event.preventDefault()
-		//post request to http://localhost:5000/api/events/add
+
+		const payload = {
+			title: titleInputRef.current!.value,
+			address: addressInputRef.current!.value,
+			description: descriptionInputRef.current!.value,
+			image: urlInputRef.current!.value,
+			hour: timeInputRef.current!.value,
+			date: dateInputRef.current!.value,
+		}
+
+		axios.post("http://localhost:5000/api/events/add", payload)
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		navigate("/");
 	}
 
 	return (
 		<div className={classes.newEventContainer}>
 			<form onSubmit={submitHandler}>
 				<h2>Add a new student activity/event idk</h2>
-				<input type="text" name="title" placeholder="Add an interesting title." required ref={titleInputRef}/>
-				<input type="text" name="address" placeholder="Meetup address." required />
-				<textarea name="description" placeholder="Short description on what's going to happen." required/>
-				<input type="url" name="img" placeholder="Url of that points at a image of the place you plan to go to." required/>
-				<input type="time" name="hour" placeholder="Hour of the meeting." required/>
-				<input type="date" name="date" placeholder="Date of the meeting" required/>
-				<input type="button" name="btn" className={classes.addEventBtn} value="Add" />
+				<input type="text" name="title" placeholder="Add an interesting title." required ref={titleInputRef} />
+				<input type="text" name="address" placeholder="Meetup address." required ref={addressInputRef} />
+				<textarea name="description" placeholder="Short description on what's going to happen." required ref={descriptionInputRef} />
+				<input type="url" name="img" placeholder="Url of that points at a image of the place you plan to go to." required ref={urlInputRef} />
+				<input type="time" name="hour" placeholder="Hour of the meeting." required ref={timeInputRef} />
+				<input type="date" name="date" placeholder="Date of the meeting" required ref={dateInputRef} />
+				<input type="submit" name="btn" className={classes.addEventBtn} value="Add" />
 			</form>
 		</div>
 	)
