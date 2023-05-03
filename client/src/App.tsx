8e76b3e5416fecfb19050b26888f2df911b8b782
kaYzz <106306274+kayYZ1@users.noreply.github.com>
2023-05-03 22:30:09 +0200

    feat: Not found route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import ViewEvents from './pages/ViewEvents'
 import NewEvent from './pages/NewEvent'
 import MarkedEvents from './pages/MarkedEvents'
+import NotFound from "./pages/NotFound" 
 
 import Layout from "./layouts/Layout"
 
@@ -13,6 +14,7 @@ const App = () => {
 			<Route path="/" element= {<ViewEvents/>} />
 			<Route path="/newEvent" element= {<NewEvent/>} />
 			<Route path="/markedEvents" element= {<MarkedEvents />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</Layout>
   )
